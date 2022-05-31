572ddd8817535c26cc84473a7a25664093789ce3
DimitriRO-IBM <dimitri.rodrigues-ISC_France@fr.ibm.com>
2022-05-31 23:48:31 +0200

    fix an issue with path

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-app.use(express.static(__dirname + '/dist/<app-name>'))
+app.use(express.static(__dirname + '/dist/dislyte-summon-battles'))
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname +
-    '/dist/dislyte-summon-battles/index.html'))
+  res.sendFile(path.join(__dirname + '/dist/dislyte-summon-battles/index.html'))
 })
 app.listen(process.env.PORT || 8080)
